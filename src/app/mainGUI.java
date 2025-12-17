e8b3b18921d77f3ae8bdba3c5d1d6b7c49b8ee9e
Aulia Oktavia Ramdani <auliaoktaviaramdani@gmail.com>
2025-12-17 10:57:55 +0700

    feature: penambahan fitur data kursus

--- src/app/mainGUI.java
+++ src/app/mainGUI.java
@@ -1,6 +1,6 @@
 package app;
 
-import gui.formPendaftaran;
+import gui.*;
 import model.*;
 
 public class mainGUI {
@@ -8,9 +8,9 @@ public class mainGUI {
         sistemPendaftaran sistem = new sistemPendaftaran("SYS001");
         
         // Tambah kursus
-        sistem.tambahKursus(new kursusBerbayar("JAV01", "Java Programming", 50, 500000));
-        sistem.tambahKursus(new kursusBerbayar("BAS02", "Basis Data", 25, 450000));
-        sistem.tambahKursus(new kursusGratis("PRB03", "Proses Bisnis", 15, 0));
+        sistem.tambahKursus(new kursusBerbayar("JAV01", "Java Programming", 5, 500000));
+        sistem.tambahKursus(new kursusBerbayar("BAS02", "Basis Data", 7, 450000));
+        sistem.tambahKursus(new kursusGratis("PRB03", "Proses Bisnis", 3, 0));
         
         // Jalankan GUI
         new formPendaftaran(sistem);
